dea4e958c69174e130f50d0397b5c00b199e89be
Wallace Peixoto <wallacepeixototi@gmail.com>
2019-12-04 18:28:46 -0300

    Endpoint /pedidos/{id} disponibilizado

--- src/main/java/com/wpl/cursomc/domain/Cliente.java
+++ src/main/java/com/wpl/cursomc/domain/Cliente.java
@@ -14,6 +14,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.wpl.cursomc.domain.enums.TipoCliente;
 
@@ -37,6 +38,7 @@ public class Cliente implements Serializable {
 	@CollectionTable(name="TELEFONE") //Define o nome da tabela no Banco de dados relacional que vai guardar os telefones
 	private Set<String> telefones = new HashSet<>();
 	
+	@JsonBackReference
 	@OneToMany(mappedBy="cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 	
